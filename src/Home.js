3a226e83e215991c8271738fb61fe9d8905432ff
seongjin <yseongjin2304@gmail.com>
2021-02-15 05:40:55 +0900

    9

--- src/Home.js
+++ src/Home.js
@@ -2,10 +2,50 @@ import React from "react";
 import { car_all } from "./queries";
 import { useQuery } from "react-apollo-hooks";
 
-export default () => {
-  const { data } = useQuery(car_all);
-  if(data){
-    console.log(data.car_table_all)
-    return <span> {data.car_table_all} </span>
+const Home = () => {
+  const { data, loading, error } = useQuery(car_all);
+
+  if (!loading && !error && data) {
+    return (
+      <div>
+        <div id="_1024_kg">
+          <span>중량: {data.car_table_all[0].weight}Kg</span>
+        </div>
+        <div id="_2024_kg">
+          <span>중량: {data.car_table_all[1].weight}Kg</span>
+        </div>
+        <div id="ent">
+          <span>입차</span>
+        </div>
+        <div id="ext">
+          <span>출차</span>
+        </div>
+        <div id="ID2021_02_7_21_03">
+          <span>{data.car_table_all[0].time}</span>
+        </div>
+        <div id="ID2021_02_7_21_03_ba">
+          <span>{data.car_table_all[1].time}</span>
+        </div>
+        <div id="_1">
+          <svg class="_1_bc">
+            <rect id="_1_bc" rx="0" ry="0" x="0" y="0" width="333" height="93">
+            </rect>
+          </svg>
+          <div id="ID14_2923">
+            <span>{data.car_table_all[1].car_number}</span>
+          </div>
+        </div>
+        <div id="__14_2923">
+          <span>차량 번호: {data.car_table_all[1].car_number}</span>
+        </div>
+        <div id="__1000kg">
+          <span>화물 중량: {data.car_table_all[1].weight - data.car_table_all[0].weight}Kg</span>
+        </div>
+      </div>
+    )
+  }
+  else {
+    return null;
   }
 }
+export default Home;
--- src/queries.js
+++ src/queries.js
@@ -5,10 +5,8 @@ export const car_all = gql`
   car_table_all{
     car_id
     car_number
-    ent_time
-    ext_time
-    ent_weight
-    ext_weight
+    time
+    weight
 	  }
   }
 `;
